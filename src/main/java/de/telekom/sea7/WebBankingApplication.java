9e0deadd0c2e7c7eb011180f66bfc8a123b8c8c5
Robert Esser <robert-esser_student2021@wilder-school>
2022-03-03 09:11:23 +0100

    erste Variante

--- src/main/java/de/telekom/sea7/WebBankingApplication.java
+++ src/main/java/de/telekom/sea7/WebBankingApplication.java
@@ -3,7 +3,6 @@ package de.telekom.sea7;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class WebBankingApplication {
 
